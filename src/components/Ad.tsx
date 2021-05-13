2413b4b4cf99e90cbb4526ab8bb19d026a45d7f4
Yitz <yhochstadt@gmail.com>
2021-05-12 22:25:37 -0400

    first commit

---
+++ src/components/Ad.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import './Ad.css';
+
+function Ad(){
+    const [flavor, setFlavor] = useState("Chocolate");
+    const [darkMode, setdarkModeOn] = useState(true);
+    const [fontSize, setFontSize] = useState(25);
+
+    function chooseChocolate(){
+        setFlavor("Chocolate");
+    }
+    function chooseVanilla(){
+        setFlavor("Vanilla");
+    }
+    function chooseMint(){
+        setFlavor("Mint");
+    }
+    let darkClass = "";
+    function darkOn(){
+        setdarkModeOn(true);
+    }
+    function darkOff(){
+        setdarkModeOn(false);
+    }
+    if(darkMode){
+        darkClass = " dark"
+    }
+    function changeFontSize(change:number):void{
+        setFontSize(prev => prev + change);
+    }
+    const styles = {
+        fontSize: fontSize + "px"
+    }
+    return(
+        <div className="ad-section">
+            <div className={"ad" + darkClass}>
+                <p>Vote For</p>
+                <p className="flavor-font" style={styles}>{flavor}</p>
+            </div>
+            <div className="ad-choices">
+                <div className="flavor">
+                    <p>What to Support</p>
+                    <button onClick={chooseChocolate}>Chocolate</button>
+                    <button onClick={chooseVanilla}>Vanilla</button>
+                    <button onClick={chooseMint}>Mint</button>   
+                </div>
+                <div className="dark-mode">
+                    <p>Color Theme</p>
+                    <button onClick={darkOn}>Dark</button>
+                    <button onClick={darkOff}>Light</button>
+                </div>
+                <p>Font Size</p>
+                <div className="font-size">
+                    <button onClick={() => changeFontSize(-1)}>Down</button>
+                    <p>{fontSize}</p>
+                    <button onClick={() => changeFontSize(+1)}>Up</button>
+                </div>                
+            </div>
+        </div>
+    )
+}
+export default Ad;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+function Header(){
+    return (
+        <div className="Header">
+            <h1>Ice Cream Wars</h1>
+        </div>
+    )
+}
+export default Header;
